2ae01af07ece4ec15364b4d1ea53081bbcfaa9eb
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-06-12 16:52:37 +0200

    scihub by title

--- getting_data/readme.md
+++ getting_data/readme.md
@@ -10,6 +10,7 @@
 * [SemEval](https://aclweb.org/aclwiki/SemEval_Portal)
 * [jiant](https://github.com/jsalt18-sentence-repl/jiant.git)
 * [ParlAI](https://github.com/facebookresearch/ParlAI)
+* [babi](https://research.fb.com/downloads/babi/)
 
 ### annotated entities
 * [GermEval2014](https://sites.google.com/site/germeval2014ner/data): german sequence tagging
@@ -26,3 +27,15 @@
 * [GermEval-2017](https://sites.google.com/view/germeval2017-absa/home): german tweets + Sentiment Analysis
 * [GermEval-2018](https://projects.fzai.h-da.de/iggsa/germeval-2018/): german Identification of Offensive Language
 * [One Million Posts Corpus](https://ofai.github.io/million-post-corpus/): German Online Discussions
+
+
+# Linguistic Data Consortium
+* [Ontonotes5](https://catalog.ldc.upenn.edu/LDC2013T19)
+  * NER: set of 18 entity types (including PERSON, NORP, FACILITY, ORGANIZATION, GPE)
+  
+* [ACE 2005 Multilingual Training Corpus](https://catalog.ldc.upenn.edu/LDC2006T06)
+  * coreference resolution
+  * entity mention detection
+  * relation extraction
+  
+* [TACRED](https://catalog.ldc.upenn.edu/LDC2018T24)
--- getting_data/scihub_scraping.py
+++ getting_data/scihub_scraping.py
@@ -6,50 +6,60 @@ from commons import data_io
 from getting_data.scholar import get_artices_from_googlescholar
 from getting_data.scihub import SciHub
 
-if __name__ == '__main__':
+data_path = '/home/tilo/data/scihub_data/'
 
+
+def dois_from_scholar_urls():
     sh = SciHub()
-    data_path = '/home/tilo/data/scihub_data/'
     no_doi_found_file = data_path + 'no_doi_found.txt'
     doi_not_in_scihub_file = data_path + 'doi_not_in_scihub.txt'
-
     no_doi_found = []
-
     doi_not_in_scihub = []
-
-    shit_counter=0
+    shit_counter = 0
     pattern = re.compile('10.\d{4,9}/[-._;()/:A-Z0-9]+$')
     articles = get_artices_from_googlescholar(phrase='emotional geographies', max_num_hits=100)
-    print('num hits %d'%len(articles))
+    print('num hits %d' % len(articles))
     for art in articles:
         url = art.attrs['url'][0]
         if any([p in url for p in [
             'https://books.google.com/books'
         ]]):
-            shit_counter+=1
+            shit_counter += 1
             continue
-    # url = 'https://journals.sagepub.com/doi/abs/10.1177/0891243287001002002'
+        # url = 'https://journals.sagepub.com/doi/abs/10.1177/0891243287001002002'
         dois = [doi for doi in pattern.findall(url)]
 
-        if len(dois)>0:
+        if len(dois) > 0:
             print(dois)
             for doi in dois:
                 try:
                     sh.download(doi, destination=data_path)
-                    print('got: %s'%doi)
+                    print('got: %s' % doi)
                 except:
                     doi_not_in_scihub.append(doi)
         else:
             no_doi_found.append(url)
-
     data_io.write_to_file(no_doi_found_file, no_doi_found, 'ab')
     data_io.write_to_file(doi_not_in_scihub_file, doi_not_in_scihub, 'ab')
+    print('shit: %d' % shit_counter)
 
-    print('shit: %d'%shit_counter)
-    # retrieve 5 articles on Google Scholars related to 'bittorrent'
-    # results = sh.search('feminism', 5)
-    # print('found %d papers'%len(results))
-    #
-    # for paper in results['papers']:
-    #     print(paper['url'])
-    #     sh.download(paper['url'],destination='/home/tilo/data/scihub_data/')
+def scihub_by_title(
+    title = 'The global and the intimate : feminism in our time'
+    ):
+
+    title_req = title.replace(' ','+')
+
+    sh = SciHub()
+    headers = {
+                 'Content-Type':'application/x-www-form-urlencoded'
+    }
+    res = sh.sess.post('https://sci-hub.tw',data='sci-hub-plugin-check=&request=%s'%title_req,headers=headers)
+    pattern = re.compile('10.\d{4,9}/[-._;()/:A-Z0-9]+')
+    dois = set(pattern.findall(res.text))
+    for doi in dois:
+        sh.download(doi, destination=data_path)
+
+if __name__ == '__main__':
+    scihub_by_title()
+    # dois_from_scholar_urls()
+
