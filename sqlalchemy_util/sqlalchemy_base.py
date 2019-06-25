c4e5050f61d4b9ad659209580d08a0c10b021537
Tilo Himmelsbach <dertilo@googlemail.com>
2019-06-25 10:26:46 +0200

    get_sqlalchemy_base_engine method

--- sqlalchemy_util/sqlalchemy_base.py
+++ sqlalchemy_util/sqlalchemy_base.py
@@ -6,3 +6,13 @@ sqlalchemy_base = declarative_base()
 POSTGRES_URL = os.environ['POSTGRES_URL'] if 'POSTGRES_URL' in os.environ else 'postgres:whocares@localhost:5432/postgres'
 sqlalchemy_engine = create_engine('postgresql://%s' %POSTGRES_URL ) if POSTGRES_URL is not None else None
 sqlalchemy_base.metadata.bind = sqlalchemy_engine
+
+def get_sqlalchemy_base_engine(POSTGRES_URL='postgres:whocares@localhost:5432/postgres',
+                               ip = None):
+    if ip is not None:
+        POSTGRES_URL = 'postgres:whocares@%s:5432/postgres'%ip
+    sqlalchemy_base = declarative_base()
+
+    sqlalchemy_engine = create_engine('postgresql://%s' % POSTGRES_URL) if POSTGRES_URL is not None else None
+    sqlalchemy_base.metadata.bind = sqlalchemy_engine
+    return sqlalchemy_base,sqlalchemy_engine
