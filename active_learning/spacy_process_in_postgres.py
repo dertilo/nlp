ffeaa6f0e1489e51e27a116d29923075bba1be44
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-06-25 18:57:15 +0200

    process postgres with spacy

--- active_learning/spacy_process_in_postgres.py
+++ active_learning/spacy_process_in_postgres.py
@@ -1,34 +1,39 @@
 import json
+from time import time
 
 import spacy
-import sqlalchemy
-from flair.models import SequenceTagger
-from sqlalchemy import Table, String, Column, select, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from torch.utils.data import Dataset
-
-from sqlalchemy_util.sqlalchemy_methods import get_tables_by_reflection, fetchemany_sqlalchemy, insert_or_update
+from sqlalchemy import select
+from sqlalchemy_util.sqlalchemy_base import get_sqlalchemy_base_engine
+from sqlalchemy_util.sqlalchemy_methods import get_tables_by_reflection, insert_or_update, fetchmany_sqlalchemy, \
+    update_batchwise
 
 if __name__ == '__main__':
-    sqlalchemy_base = declarative_base()
     ip = '10.1.1.29'
-    sqlalchemy_engine = create_engine('postgresql://%s' % 'postgres:whocares@%s:5432/postgres'%ip)
-    sqlalchemy_base.metadata.bind = sqlalchemy_engine
+    sqlalchemy_base,sqlalchemy_engine = get_sqlalchemy_base_engine(ip=ip)
 
     spacy_nlp = spacy.load('en_core_web_sm')
 
     arxiv_table = get_tables_by_reflection(sqlalchemy_base.metadata,sqlalchemy_engine)['arxiv']
-    scierc_table = get_tables_by_reflection(sqlalchemy_base.metadata,sqlalchemy_engine)['scierc']
+    # scierc_table = get_tables_by_reflection(sqlalchemy_base.metadata,sqlalchemy_engine)['scierc']
 
     q = select([arxiv_table.c.id,arxiv_table.c.pdf_full_text]).where(arxiv_table.c.pdf_full_text.isnot(None))
     def process_to_str(doc):
         return json.dumps([[tok.text for tok in sent]for sent in doc.sents])
-
+    batch_size = 1000
+    start = time()
     with sqlalchemy_engine.connect() as conn:
-        for batch in fetchemany_sqlalchemy(sqlalchemy_engine, q, batch_size=10):
+        for batch in fetchmany_sqlalchemy(sqlalchemy_engine, q, batch_size=10):
             spacy_docs = [(d['id'], spacy_nlp(json.loads(d['pdf_full_text']))) for d in batch]
-            ids_sentences = [{'id':eid,'sentences':process_to_str(doc)} for eid, doc in spacy_docs]
-            insert_or_update(conn,scierc_table,['sentences'],ids_sentences)
-            break
+            ids_sentences = [{'id':eid.replace('/','').replace(':',''),'tokenizd_sentences':process_to_str(doc)} for eid, doc in spacy_docs]
+            insert_or_update(conn,arxiv_table,['tokenized_sentences'],ids_sentences)
+    print('old method took: %0.2f secs'%(time()-start))
+    def process_fun(batch):
+        spacy_docs = [(d['id'], spacy_nlp(json.loads(d['pdf_full_text']))) for d in batch]
+        processed_docs = [{'id': eid.replace('/', '').replace(':', ''), 'tokenized_sentences': process_to_str(doc)} for eid, doc in
+                         spacy_docs]
+        return processed_docs
 
+    start = time()
+    update_batchwise(sqlalchemy_engine, select([arxiv_table]), arxiv_table, process_fun, batch_size=batch_size)
+    print('new method took: %0.2f secs'%(time()-start))
 
