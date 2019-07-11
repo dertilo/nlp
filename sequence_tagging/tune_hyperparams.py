c2cf1ca66567ca996fbb182b86933c8a28b545ab
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-07-11 18:56:53 +0200

    tune hyperparams with ray

---
+++ sequence_tagging/tune_hyperparams.py
@@ -0,0 +1,71 @@
+import multiprocessing
+import sys
+from time import time
+
+sys.path.append('.')
+
+import json
+from functools import partial
+
+from commons import data_io
+from ray.tune.suggest.hyperopt import HyperOptSearch
+from sklearn.model_selection import ShuffleSplit
+
+from ray import tune
+from ray.tune import track, Trainable
+
+from model_evaluation.crossvalidation import calc_mean_std_scores
+from sequence_tagging.benchmark_taggers import score_spacycrfsuite_tagger
+from sequence_tagging.flair_scierc_ner import build_flair_sentences
+
+import os
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_data():
+    # data_path = '/home/tilo/code/NLP/scisci_nlp/data/scierc_data/json'
+    # data_path = '/docker-share/scierc_data/json'
+    data_path = '/home/users/t/tilo-himmelsbach/data/scierc_data/json'
+    sentences = [sent for jsonl_file in ['train.json', 'dev.json', 'test.json']
+                 for d in data_io.read_jsons_from_file('%s/%s' % (data_path, jsonl_file))
+                 for sent in build_flair_sentences(d)]
+    return sentences
+
+data = None
+def train_mnist(config):
+    global data
+    if data is None:
+        data = get_data()
+
+    splitter = ShuffleSplit(n_splits=3, test_size=0.2, random_state=111)
+    splits = [(train, train[:10], test) for train, test in splitter.split(X=range(len(data)))]
+    m_scores_std_scores = calc_mean_std_scores(data, partial(score_spacycrfsuite_tagger, params=config), splits)
+
+    track.log(f1=m_scores_std_scores['m_scores']['f1-test'])
+
+
+if __name__ == "__main__":
+    from hyperopt import hp
+    import numpy as np
+    space = {
+        'c1': hp.loguniform('c1', np.log(10) * -6, 0),
+        'c2': hp.loguniform('c2', np.log(10) * -6, 0),
+    }
+
+    algo = HyperOptSearch(space, max_concurrent=multiprocessing.cpu_count()-1,metric='f1')
+    start = time()
+    trials = tune.run(
+        train_mnist,
+        search_alg=algo,
+        name="exp",
+        stop={
+            "training_iteration": 1
+        },
+        num_samples=9,
+        reuse_actors=False
+    )
+    print('hyperparam-tuning took: %0.2f seconds'%(time()-start))
+    for trial in trials:
+        t =trial.last_result
+        print('date: %s; f1-micro: %0.2f; params: %s' % (
+        t['date'], t['f1'], json.dumps(t['config'])))
